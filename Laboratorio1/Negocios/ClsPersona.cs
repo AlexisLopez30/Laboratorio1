306ee88987e59422b0e87af3077fb8418d31a7d1
@AlexisLopez30 <lc0325032022@unab.edu.sv>
2023-03-01 17:43:11 -0600

    Laboratorio1

---
+++ Laboratorio1/Negocios/ClsPersona.cs
@@ -0,0 +1,38 @@
+﻿using Laboratorio1.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio1.Negocios
+{
+    public class ClsPersona
+    {
+        public String CalcularIMC(Persona per)
+        {
+            double Promedio = per.Peso / Math.Pow(per.Altura, 2);
+
+            if (Promedio > 25)
+            {
+                return $"tiene sobre peso";
+            }
+            else
+            {
+                return $"tiene un peso ideal";
+            }
+        }
+        public String MayorDeEdad(Persona per)
+        {
+            if (per.Edad >= 18)
+            {
+                return $"Es mayor de edad";
+            }
+            else
+            {
+                return $"Es menor de edad";
+            }
+        }
+    }
+}
---
+++ Laboratorio1/Program.cs
@@ -0,0 +1,28 @@
+﻿using Laboratorio1.Entidades;
+using Laboratorio1.Negocios;
+
+Persona per = new Persona();
+ClsPersona clsPersona = new ClsPersona();
+
+Console.Write(@"Bienvenido 
+Ingrese su Nombre por favor -> ");
+per.Nombre = Console.ReadLine();
+
+Console.Write("\nIngrese su edad actual -> ");
+per.Edad = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\nIngrese su sexo -> ");
+per.Sexo = Console.ReadLine();
+
+Console.Write("\nIngrese su peso en Kg. (kilogramos) -> ");
+per.Peso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("\nIngrese su altura en metros -> ");
+per.Altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($@"
+Nombre: {per.Nombre} edad: {per.Edad} sexo: {per.Sexo}
+Peso: {per.Peso}
+Altura: {per.Altura}
+Usted {clsPersona.CalcularIMC(per)}
+Usted {clsPersona.MayorDeEdad(per)}");
